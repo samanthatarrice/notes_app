9fb605a7dae35a6e431dc4be0f43b922470b824f
Samantha Tarrice <s.tarrice@gmail.com>
2021-11-13 18:27:54 -0300

    Close icon hides modal

--- script.js
+++ script.js
@@ -14,39 +14,45 @@
 
 // $('i.far.fa-trash-alt').click(console.log('working'));
 
-$(document).ready(function(){
 
-  $('.add-note').click(newSticky);
 
-  $(document).on('click', '.fa-trash-alt', function() {
-    $(this).parent().remove();
-  }) //needed to use .on instead of .click in order to access dynamically created elements from other functions
+$('.add-note').click(newSticky);
 
-  $(document).on('click', '.fa-search-plus', function() {
-    const modalBackground = $('<div class="modal-background"></div>');
-    const modalContent = $('<div class="modal-content"></div>');
-    const close = $('<i class="fas fa-window-close"></i>');
+$(document).on('click', '.fa-trash-alt', function() {
+  $(this).parent().remove();
+}) //needed to use .on instead of .click in order to access dynamically created elements from other functions
 
-    $('body').append(modalBackground);
-    modalBackground.append(modalContent);
-    modalContent.append(close);
-  })  
+$(document).on('click', '.fa-search-plus', openModal);
 
-  function newSticky() {
-    const stickyContainer = $('<div class="sticky-container"></div>');
-    const stickyImg = $('<img src="images/sticky-note.png" />');
-    const stickyPEl = $('<p></p>');
-    const trash = $('<i class="far fa-trash-alt"></i>');
-    const enlarge = $('<i class="fas fa-search-plus"></i>');
-    
-    $('.stickies').append(stickyContainer);
-    stickyContainer.append(stickyImg);
-    stickyContainer.append(stickyPEl.text($("#new-note").val()));
-    stickyContainer.append(trash);
-    stickyContainer.append(enlarge);
-  }
+function newSticky() {
+  const stickyContainer = $('<div class="sticky-container"></div>');
+  const stickyImg = $('<img src="images/sticky-note.png" />');
+  const stickyPEl = $('<p></p>');
+  const trash = $('<i class="far fa-trash-alt"></i>');
+  const enlarge = $('<i class="fas fa-search-plus"></i>');
+  
+  $('.stickies').append(stickyContainer);
+  stickyContainer.append(stickyImg);
+  stickyContainer.append(stickyPEl.text($("#new-note").val()));
+  stickyContainer.append(trash);
+  stickyContainer.append(enlarge);
+}
 
-})
+function openModal() {
+  const modalBackground = $('<div class="modal-background"></div>');
+  const modalContent = $('<div class="modal-content"></div>');
+  const close = $('<i class="fas fa-window-close"></i>');
+
+  $('body').append(modalBackground);
+  modalBackground.append(modalContent);
+  modalContent.append(close);
+
+  $(document).on('click', '.fa-window-close', () => {
+    modalBackground.css('display', 'none');
+  });
+}
+
+
 
 // function deleteSticky() {
 //   alert('working');

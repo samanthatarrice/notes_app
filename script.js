c51cef90a97f922b559560e70ad9e954a0d4c146
Samantha Tarrice <s.tarrice@gmail.com>
2021-11-13 16:14:19 -0300

    Add trash images from Font Awesome

--- script.js
+++ script.js
@@ -14,14 +14,20 @@
 
 $('.add-note').click(newSticky);
 
+$('i.far.fa-trash-alt').click(console.log('working'));
+
 function newSticky() {
   const stickyContainer = $('<div class="sticky-container"></div>');
   const stickyImg = $('<img src="images/sticky-note.png" />');
   const stickyPEl = $('<p></p>');
-  // const delete = $('')
+  const trash = $('<i class="far fa-trash-alt"></i>');
   
-
   $('.stickies').append(stickyContainer);
   stickyContainer.append(stickyImg);
   stickyContainer.append(stickyPEl.text($("#new-note").val()));
+  stickyContainer.append(trash);
 }
+
+// function deleteSticky() {
+//   alert('working');
+// }
